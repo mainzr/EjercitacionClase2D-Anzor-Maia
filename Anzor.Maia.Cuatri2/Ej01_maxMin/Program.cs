f2195270875205e7ed438b60a9c87ff4c303d102
Maia Anzor <mainzr>
2022-08-22 22:21:29 -0300

    commit

---
+++ Anzor.Maia.Cuatri2/Ej01_maxMin/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ej01_maxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeros = 0;
+            int flag = 0;
+            int flagMayor = 0;
+            float promedio = 0;
+            int acumulador = 0;
+
+            int max = 0;
+            int min = 0;
+
+            //Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("ingrese los numeros:");
+                numeros = int.Parse(Console.ReadLine());
+
+                acumulador += numeros;
+
+                if(flag == 0 || min > numeros)
+                {
+                    min = numeros;
+                    flag = 1;
+                }
+
+                if(flagMayor == 0 || max < numeros)
+                {
+                    max = numeros;
+                    flagMayor = 1;
+                }
+
+            }
+            promedio = acumulador / 5;
+
+
+            Console.WriteLine($"Maximo: {max} - minimo: {min} - promedio: {promedio}");
+
+        }
+    }
+}
---
+++ Anzor.Maia.Cuatri2/ej-02-ErrorAlCubo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ej_02_ErrorAlCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un número y mostrar el cuadrado y el cubo del mismo.
+            //Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!"
+
+            int numero;
+            double cuadrado;
+            double cubo;
+
+            Console.WriteLine("ingrese el numero:");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero < 0)
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+
+            cuadrado = Math.Pow(numero, numero);
+            cubo = Math.Pow(numero, 3);
+
+            Console.WriteLine($"Numero al cubo: {cubo} - numero al cuadrado: {cuadrado}");
+        }
+    }
+}
