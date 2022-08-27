de64b4a1073e22f6ab2db5a70a03aa54b627489d
Maia Anzor <mainzr>
2022-08-27 19:15:04 -0300

    ultimos ejercicios subidos

---
+++ Anzor.Maia.Cuatri2/Ej05-Entidades-POO/Rectangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej05_Entidades_POO
+{
+    public class Rectangulo
+    {
+        private float area;
+        private float perimetro;
+        Punto vertice1;
+        Punto vertice2;
+        Punto vertice3;
+        Punto vertice4;
+
+        public Rectangulo(Punto vertice1, Punto vertice3)
+        {
+            this.vertice1 = vertice1;
+            this.vertice3 = vertice3;
+
+            this.vertice2 = new Punto(vertice3.GetX(), vertice1.GetY());
+            this.vertice4 = new Punto(vertice1.GetX(), vertice3.GetY());
+
+        }
+        public float GetPerimetro()
+        {
+            //((base + altura) * 2)
+            float baseResultado;
+            baseResultado = this.vertice4.GetX() - this.vertice3.GetX();
+
+            float alturaResultado;
+            alturaResultado = this.vertice4.GetY() - this.vertice1.GetY();
+
+            this.perimetro = Math.Abs((baseResultado + alturaResultado) * 2);
+
+            return this.perimetro;
+        }
+
+        public float GetArea()
+        {
+            //area base* altura
+            //punto x v4 - punto x v3
+
+            float baseResultado;
+            baseResultado = this.vertice4.GetX() - this.vertice3.GetX();
+
+            float alturaResultado;
+            alturaResultado = this.vertice4.GetY() - this.vertice1.GetY();
+
+            this.area = Math.Abs(baseResultado) * Math.Abs(alturaResultado);
+
+            return this.area;
+        }
+    }
+
+
+}
---
+++ Anzor.Maia.Cuatri2/Ej05-PruebaDeGeometria/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using Ej05_Entidades_POO;
+
+namespace Ej05_PruebaDeGeometria
+{
+    class Program
+    {
+        public static string Mostrar(Rectangulo rectanguloHijo)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            resultado.AppendLine($"AREA: {rectanguloHijo.GetArea()}");
+            resultado.AppendLine($"PERIMETRO: {rectanguloHijo.GetPerimetro()}");
+
+            return resultado.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo(new Punto(2, 3), new Punto(7, -1));
+
+            Console.WriteLine(Mostrar(rectangulo));
+        }
+    }
+}
